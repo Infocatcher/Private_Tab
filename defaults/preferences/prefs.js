04b2ed0af3ab97d9f0ae23eeab4b24b56b96e565
Infocatcher <infocatcher.dev.null@gmail.com>
2013-01-14 00:59:59 +0400

    Group preferences by "type"

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -2,9 +2,12 @@ pref("extensions.privateTab.loadInBackground", -1);
 // -1 - use built-in browser.tabs.loadInBackground
 // 0  - load in foreground
 // 1  - load in background
-pref("extensions.privateTab.workaroundForPendingTabs", true);
-pref("extensions.privateTab.dontUseTabsInPopupWindows", true);
+
 pref("extensions.privateTab.keysIgnoreDefaultPrevented", false); // Ignore, if someone stops "keypress" event
 pref("extensions.privateTab.key.openNewPrivateTab", "control alt p");
 pref("extensions.privateTab.key.toggleTabPrivate", "control alt v");
+
+pref("extensions.privateTab.workaroundForPendingTabs", true);
+pref("extensions.privateTab.dontUseTabsInPopupWindows", true);
+
 pref("extensions.privateTab.debug", false);
