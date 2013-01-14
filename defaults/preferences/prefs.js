31d5acdc6ef4ad0720ace8c392c5886f98e2bb22
Infocatcher <infocatcher.dev.null@gmail.com>
2013-01-14 12:04:20 +0400

    Add link to hotkeys syntax documentation

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -4,6 +4,8 @@ pref("extensions.privateTab.loadInBackground", -1);
 // 1  - load in background
 
 pref("extensions.privateTab.keysIgnoreDefaultPrevented", false); // Ignore, if someone stops "keypress" event
+// See https://developer.mozilla.org/en-US/docs/XUL/Tutorial/Keyboard_Shortcuts
+// Syntax: [<modifiers> ]<key or keycode>
 pref("extensions.privateTab.key.openNewPrivateTab", "control alt p");
 pref("extensions.privateTab.key.toggleTabPrivate", "control alt v");
 
