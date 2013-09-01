Infocatcher/Private_Tab

commit 4ca9d51e7ae96c49bd4ab54c9a07c9bdadada01c

Correct styles to show button after last tab, if used Tab Mix Plus with multi-row tab bar (fixes #96)

bootstrap.js
@@ -2804,16 +2804,26 @@ var windowsObserver = {
 				}\n\
 				/*\n\
 				Show button after last tab for [Tabs][New Tab][New Private Tab] and [Tabs][New Private Tab]\n\
-				And also show "New Tab" after last tab for [Tabs][New Private Tab][New Tab]\n\
+				and also show "New Tab" after last tab for [Tabs][New Private Tab][New Tab]\n\
+				Includes "tabmixScrollBox" for Tab Mix Plus https://addons.mozilla.org/addon/tab-mix-plus/
 				*/\n\
 				#' + this.afterTabsButtonId + ',\n\
 				#TabsToolbar[currentset*="tabbrowser-tabs,new-tab-button,' + this.toolbarButtonId + '"]\n\
 					> #tabbrowser-tabs:not([overflow="true"])\n\
 					~ #' + this.toolbarButtonId + ',\n\
+				#TabsToolbar[currentset*="tabmixScrollBox,new-tab-button,' + this.toolbarButtonId + '"]\n\
+					> #tabbrowser-tabs:not([overflow="true"])\n\
+					~ #' + this.toolbarButtonId + ',\n\
 				#TabsToolbar[currentset*="tabbrowser-tabs,' + this.toolbarButtonId + '"]\n\
 					> #tabbrowser-tabs:not([overflow="true"])\n\
 					~ #' + this.toolbarButtonId + ',\n\
+				#TabsToolbar[currentset*="tabmixScrollBox,' + this.toolbarButtonId + '"]\n\
+					> #tabbrowser-tabs:not([overflow="true"])\n\
+					~ #' + this.toolbarButtonId + ',\n\
 				#TabsToolbar[currentset*="tabbrowser-tabs,' + this.toolbarButtonId + ',new-tab-button"]\n\
+					> #tabbrowser-tabs:not([overflow="true"])\n\
+					~ #new-tab-button,\n\
+				#TabsToolbar[currentset*="tabmixScrollBox,' + this.toolbarButtonId + ',new-tab-button"]\n\
 					> #tabbrowser-tabs:not([overflow="true"])\n\
 					~ #new-tab-button {\n\
 					visibility: collapse !important;\n\
@@ -2821,10 +2831,19 @@ var windowsObserver = {
 				#TabsToolbar[currentset*="tabbrowser-tabs,new-tab-button,' + this.toolbarButtonId + '"]:not([customizing="true"])\n\
 					> #tabbrowser-tabs:not([overflow="true"])\n\
 					#' + this.afterTabsButtonId + ',\n\
+				#TabsToolbar[currentset*="tabmixScrollBox,new-tab-button,' + this.toolbarButtonId + '"]:not([customizing="true"])\n\
+					> #tabbrowser-tabs:not([overflow="true"])\n\
+					#' + this.afterTabsButtonId + ',\n\
 				#TabsToolbar[currentset*="tabbrowser-tabs,' + this.toolbarButtonId + '"]:not([customizing="true"])\n\
 					> #tabbrowser-tabs:not([overflow="true"])\n\
 					#' + this.afterTabsButtonId + ',\n\
+				#TabsToolbar[currentset*="tabmixScrollBox,' + this.toolbarButtonId + '"]:not([customizing="true"])\n\
+					> #tabbrowser-tabs:not([overflow="true"])\n\
+					#' + this.afterTabsButtonId + ',\n\
 				#TabsToolbar[currentset*="tabbrowser-tabs,' + this.toolbarButtonId + ',new-tab-button"]:not([customizing="true"])\n\
+					> #tabbrowser-tabs:not([overflow="true"])\n\
+					.tabs-newtab-button[command="cmd_newNavigatorTab"],\n\
+				#TabsToolbar[currentset*="tabmixScrollBox,' + this.toolbarButtonId + ',new-tab-button"]:not([customizing="true"])\n\
 					> #tabbrowser-tabs:not([overflow="true"])\n\
 					.tabs-newtab-button[command="cmd_newNavigatorTab"] {\n\
 					visibility: visible !important;\n\

commit 260284527c770a9b61f36c79403b1e1e76c8b485

Fix not escaped line break (introduced in 4ca9d51e7ae96c49bd4ab54c9a07c9bdadada01c) (#96)

bootstrap.js
@@ -2805,7 +2805,7 @@ var windowsObserver = {
 				/*\n\
 				Show button after last tab for [Tabs][New Tab][New Private Tab] and [Tabs][New Private Tab]\n\
 				and also show "New Tab" after last tab for [Tabs][New Private Tab][New Tab]\n\
-				Includes "tabmixScrollBox" for Tab Mix Plus https://addons.mozilla.org/addon/tab-mix-plus/
+				Includes "tabmixScrollBox" for Tab Mix Plus https://addons.mozilla.org/addon/tab-mix-plus/\n\
 				*/\n\
 				#' + this.afterTabsButtonId + ',\n\
 				#TabsToolbar[currentset*="tabbrowser-tabs,new-tab-button,' + this.toolbarButtonId + '"]\n\