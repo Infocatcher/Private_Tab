2d233b14c4a36daf897b07a9a4054fe207f4fa8f
Infocatcher <infocatcher.dev.null@gmail.com>
2016-02-29 01:07:52 +0300

    Delete all global variables from "unloaded" process script (#211)

--- protocol-process.js
+++ protocol-process.js
@@ -2,10 +2,12 @@ Components.utils.import("resource://gre/modules/Services.jsm");
 if(Services.appinfo.processType == Services.appinfo.PROCESS_TYPE_CONTENT) {
 	Components.utils.import("chrome://privatetab/content/protocol.jsm");
 	privateProtocol.init(function() {});
+	let global = this;
 	addMessageListener("PrivateTab:ProtocolDestroy", function destroy(msg) {
 		removeMessageListener("PrivateTab:ProtocolDestroy", destroy);
 		privateProtocol.destroy();
-		privateProtocol = null;
+		delete global.Services;
+		delete global.privateProtocol;
 		Components.utils.unload("chrome://privatetab/content/protocol.jsm");
 	});
 }
