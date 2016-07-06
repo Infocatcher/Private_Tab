55d0ff9ce72c6d1845bfbc87f2a6daa1153a64c3
Infocatcher <infocatcher.dev.null@gmail.com>
2016-07-06 10:58:17 +0300

    Clarify logic: don't create global variables at all

--- protocol-process.js
+++ protocol-process.js
@@ -1,13 +1,12 @@
-Components.utils.import("resource://gre/modules/Services.jsm");
-if(Services.appinfo.processType == Services.appinfo.PROCESS_TYPE_CONTENT) {
-	Components.utils.import("chrome://privatetab/content/protocol.jsm");
+(function() {
+	var {Services} = Components.utils.import("resource://gre/modules/Services.jsm", {});
+	if(Services.appinfo.processType != Services.appinfo.PROCESS_TYPE_CONTENT)
+		return;
+	var {privateProtocol} = Components.utils.import("chrome://privatetab/content/protocol.jsm", {});
 	privateProtocol.init(function() {});
-	let global = this;
 	addMessageListener("PrivateTab:ProtocolDestroy", function destroy(msg) {
 		removeMessageListener("PrivateTab:ProtocolDestroy", destroy);
 		privateProtocol.destroy();
-		delete global.Services;
-		delete global.privateProtocol;
 		Components.utils.unload("chrome://privatetab/content/protocol.jsm");
 	});
-}
+})();
