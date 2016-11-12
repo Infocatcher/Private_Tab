45606206fdc18aa8734f39c90cc429b1241311be
Infocatcher <infocatcher.dev.null@gmail.com>
2016-11-12 21:42:39 +0300

    Optimize: load content.jsm only into remote tabs Messaging like messageManager.sendAsyncMessage() isn't needed (and isn't used) for non-remote tabs (related to #235)

--- content.js
+++ content.js
@@ -1,1 +1,5 @@
-new (Components.utils.import("chrome://privatetab/content/content.jsm", {}).PrivateTabContent)(this);
+{
+	let {Services} = Components.utils.import("resource://gre/modules/Services.jsm", {});
+	if(Services.appinfo.processType == Services.appinfo.PROCESS_TYPE_CONTENT)
+		new (Components.utils.import("chrome://privatetab/content/content.jsm", {}).PrivateTabContent)(this);
+}
