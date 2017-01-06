8dc7b8ff094e310bb4d06d24bf32e81e742a3898
Infocatcher <infocatcher.dev.null@gmail.com>
2017-01-06 21:39:33 +0300

    Don't use deprecated Date.prototype.toLocaleFormat() https://bugzilla.mozilla.org/show_bug.cgi?id=818634

--- bootstrap.js
+++ bootstrap.js
@@ -4818,7 +4818,7 @@ var _dbg = true, _dbgv = true;
 function ts() {
 	var d = new Date();
 	var ms = d.getMilliseconds();
-	return d.toLocaleFormat("%M:%S:") + "000".substr(String(ms).length) + ms + " ";
+	return d.toTimeString().replace(/^.*\d+:(\d+:\d+).*$/, "$1") + ":" + "000".substr(("" + ms).length) + ms + " ";
 }
 function _log(s) {
 	if(!_dbg)
