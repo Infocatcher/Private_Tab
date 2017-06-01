dee8bc6ea15b550e8cb533a46df178375bfd58dc
multiSnow <none@none.none>
2017-06-01 20:59:51 +0800

    error if xpi already exists

--- make.sh
+++ make.sh
@@ -5,6 +5,15 @@ VER=`sed -nr 's%^\s*<em:version>([.0-9a-z]+)</em:version>\s*$%\1%p' install.rdf`
 XPI=private_tab-${VER}.xpi
 FILES='install.rdf *.manifest *.js *.jsm *.xul *.xml *.html *.css license* *.png defaults modules components locale chrome idl'
 
+if test -f ${XPI}; then
+    echo title Error - Make ${XPI}
+    echo =============================================
+    echo Something went wrong, please remove or rename
+    echo ${XPI}
+    echo =============================================
+    exit 1
+fi
+
 for z in 7za 7z zip
 do
 	if command -v $z >/dev/null 2>&1
