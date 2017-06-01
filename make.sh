86160d624e55d667e1fd1b5a106e3e330ae78714
multiSnow <none@none.none>
2017-06-01 21:02:12 +0800

    code style of make.sh

--- make.sh
+++ make.sh
@@ -4,6 +4,7 @@ PROG=false
 VER=`sed -nr 's%^\s*<em:version>([.0-9a-z]+)</em:version>\s*$%\1%p' install.rdf`
 XPI=private_tab-${VER}.xpi
 FILES='install.rdf *.manifest *.js *.jsm *.xul *.xml *.html *.css license* *.png defaults modules components locale chrome idl'
+PROGS='7za 7z zip'
 
 if test -f ${XPI}; then
     echo title Error - Make ${XPI}
@@ -14,26 +15,22 @@ if test -f ${XPI}; then
     exit 1
 fi
 
-for z in 7za 7z zip
-do
-	if command -v $z >/dev/null 2>&1
-	then
-	PROG=$z
-	break
-	fi
+for z in ${PROGS}; do
+    if which ${z} > /dev/null 2>&1; then
+        PROG=${z}
+        break
+    fi
 done
 
-if test $PROG = false
-then
-	echo "usable program not found, abort."
-	exit 1
+if test ${PROG} = false; then
+    echo "usable program not found, abort."
+    exit 1
 else
-	echo "use $PROG to create xpi."
+    echo "use ${PROG} to create xpi."
 fi
 
-if test $PROG = zip
-then
-	$PROG -9r $XPI $FILES
+if test ${PROG} = zip; then
+    ${PROG} -9r ${XPI} ${FILES}
 else
-	$PROG a -tzip -mm=Deflate -mx9 -mfb258 -mpass=15 $XPI $FILES
+    ${PROG} a -tzip -mm=Deflate -mx9 -mfb258 -mpass=15 ${XPI} ${FILES}
 fi
